7defe824f0d7f66c1a89095a7969fccd1fb8f9c2
Julia <log6117@yandex.ru>
2023-06-13 23:01:21 +0300

    написала программу

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 1.
+// Напишите программу, кот. на вход принимает число и выдает его квадрат.
+
+Console.WriteLine ("input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine ($"корень из вашего числа: {result}");
+
